aa43c043f78f93802067041801341870a4ec8fb7
davideaded <david.castrodc033@gmail.com>
2025-05-18 19:55:20 -0300

    clean up

--- nvim/lua/core/plugin_config/toggleterm.lua
+++
@@ -1,7 +0,0 @@
-require("toggleterm").setup{
-	open_mapping = '<C-l>',
-	direction = 'horizontal',
-	shade_terminals = true,
-}
-
-vim.api.nvim_set_keymap('n', '<C-p>', ':ToggleTerm direction=vertical size=55<CR>', { noremap = true, silent = true })
